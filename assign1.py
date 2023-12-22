dc9f01357918e25739aa06f3192644d2ff057837
maunee29 <maunee98@my.yorku.ca>
2023-12-21 21:07:21 -0500

    This is first commiit by Maunee

---
+++ assign1.py
@@ -0,0 +1,20 @@
+
+def printNamesAlphabetically(file_path):
+    #filepath = input("Insert filepath: ")
+    # print("Insert filepath: ")
+    #file = open(filepath, 'r')
+  #  with open(file_path, 'r') as file:
+     #   names = file.readlines()
+   # with open(filepath, "r") as file:
+    #   fileNames = file.readlines()
+   # sortedFile = sorted(file)
+    #print(sortedFile)
+
+#   with open(file_path, 'r') as file:
+ #      data = file.read()
+  # return data
+
+  with open(, 'r') as file
+    line = open_file.readline()
+
+
